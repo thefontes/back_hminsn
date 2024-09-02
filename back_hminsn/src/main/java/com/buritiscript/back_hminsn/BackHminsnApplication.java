68463ac659e8597822cbf8730ec5e85915e595d3
Emerson reis <70235882+Emerson66@users.noreply.github.com>
2024-09-02 13:01:32 -0400

    Add files via upload

---
+++ back_hminsn/src/main/java/com/buritiscript/back_hminsn/BackHminsnApplication.java
@@ -0,0 +1,13 @@
+package com.buritiscript.back_hminsn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackHminsnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackHminsnApplication.class, args);
+	}
+
+}
